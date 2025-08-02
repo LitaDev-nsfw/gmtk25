3fb24fbc4f8977c94fa0f5d7154adecfcd9f9c91
Tiago Araújo <59618151+Guito14@users.noreply.github.com>
2025-08-02 18:35:38 +0100

    Settings Menu not totaly fine
    falta perceber o porque de não ficar interagível

--- gmtk25/Pause_Menu/pause_menu.gd
+++ gmtk25/Pause_Menu/pause_menu.gd
@@ -1,7 +1,10 @@
 extends Control
+@onready var settings: Control = $"../Settings"
+@onready var pause_menu: Control = $"."
 
 func _ready():
 	$AnimationPlayer.play("RESET")
+	settings.visible = false
 	hide()
 
 	
@@ -9,11 +12,14 @@ func resume():
 	get_tree().paused = false
 	$AnimationPlayer.play_backwards("Blur")
 	hide()
+	settings.visible = false
+
 	
 
 func pause():
 	$AnimationPlayer.play("Blur")
 	get_tree().paused = true
+	settings.visible = false
 	show()
 	
 
@@ -32,6 +38,10 @@ func _on_resume_pressed() -> void:
 func _on_restart_pressed() -> void:
 	resume()
 	get_tree().reload_current_scene()
+	
+func _on_settings_pressed() -> void:
+	settings.visible = true
+	pause_menu.visible = false
 
 
 func _on_quit_pressed() -> void:
@@ -39,7 +49,3 @@ func _on_quit_pressed() -> void:
 	
 func _process(delta):
 	escape()
-
-
-func _on_settings_pressed() -> void:
-	pass # Replace with function body.
